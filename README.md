e35f2b9792040d0699d732e659698a5b5bb9f893
sspeedwagonn <burnerforcarter@gmail.com>
2024-09-26 20:18:21 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Oraxen Configuration Generator
+
+Generate item configuration files with ease! Coded as a solution to avoid the hassle of copy-and-pasting a template or worse -- writing it out new each time (who does that?!).
+
+## Use
+1. Download oraxenconfiggen.py by downloaded the file directly, cloning the repository, or whatever other way you prefer.
+2. Open up the file and change values that have "# CHANGE" next to them. You can also edit the templates to your liking, but it's not required.
+3. Run the program using "py oraxenconfiggen.py"
+4. Click "Open Folder" and open the folder that holds the target models
+5. Click on each model and indicate if they should be sittable or not.
+6. Click "Generate Configuration File" and choose the name of the file (w/o '.yml'), the starting custom model data number, and finally, the file where the config should go (ideally plugins/oraxen/items)
+
+## Future Plans
+In the future, this generator will be completely configurable from the very start without having to interact with the code.
+
+Enjoy!
---
+++ oraxenconfiggen.py
@@ -0,0 +1,154 @@
+import os
+import tkinter as tk
+from tkinter import filedialog, simpledialog, messagebox
+
+'''
+Step 1: Defining Item Templates
+
+These are the templates I personally use.
+
+Future commits will implement additional customizeability with the values available. For now, I've added base values that work for my server. If you find yourself using this before that's added, make sure to change it to base values that work for your server!
+
+Config templates are divided by item and sittable. The item template lacks a "seat" value whilst sittable has one.
+'''
+item_template = """
+{item_name}:
+  itemname: <white>{display_name}
+  material: PAPER
+  Mechanics:
+    furniture:
+      limited_placing:
+        roof: false
+        floor: true
+        wall: false
+      barrier: false
+      type: ITEM_FRAME
+      drop:
+        silktouch: false
+        loots:
+        - oraxen_item: {item_name}
+          probability: 1
+  Pack:
+    generate_model: false
+    model: {model_path}/{item_name}
+    custom_model_data: {custom_model_data}
+"""
+
+sittable_template = """
+{item_name}:
+  itemname: <white>{display_name}
+  material: PAPER
+  Mechanics:
+    furniture:
+      limited_placing:
+        roof: false
+        floor: true
+        wall: false
+      barrier: false
+      seat:
+        height: -0.1
+        yaw: 0
+      type: ITEM_FRAME
+      drop:
+        silktouch: false
+        loots:
+        - oraxen_item: {item_name}
+          probability: 1
+  Pack:
+    generate_model: false
+    model: {model_path}/{item_name}
+    custom_model_data: {custom_model_data}
+"""
+
+class OraxenConfigGen:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Oraxen Configuration Generator")
+        self.select_folder_button = tk.Button(root, text="Select Folder", command=self.select_folder)
+        self.select_folder_button.pack(pady=10)
+        self.file_listbox = tk.Listbox(root, selectmode=tk.MULTIPLE, width=50, height=15)
+        self.file_listbox.pack(pady=10)
+        self.chairs_button = tk.Button(root, text="Mark as Sittable", command=self.mark_as_chairs)
+        self.chairs_button.pack(side=tk.LEFT, padx=10)
+        self.non_chairs_button = tk.Button(root, text="Mark as Item", command=self.mark_as_non_chairs)
+        self.non_chairs_button.pack(side=tk.RIGHT, padx=10)
+        self.items = {}
+        self.custom_model_data = 0 #CHANGE: Default starting number
+        self.selected_folder = ""
+        self.generate_yaml_button = tk.Button(root, text="Generate Configuration File", command=self.generate_config_file)
+        self.generate_yaml_button.pack(pady=20)
+
+    def select_folder(self):
+        self.selected_folder = filedialog.askdirectory()
+        if self.selected_folder:
+            self.populate_file_list(self.selected_folder)
+
+    def populate_file_list(self, folder_path):
+        self.file_listbox.delete(0, tk.END)
+        self.items.clear()
+
+        for filename in os.listdir(folder_path):
+            if filename.endswith(".json"):
+                item_name = os.path.splitext(filename)[0]
+                self.file_listbox.insert(tk.END, item_name)
+
+    def mark_as_chairs(self):
+        selected_files = self.file_listbox.curselection()
+        for index in selected_files:
+            item_name = self.file_listbox.get(index)
+            self.items[item_name] = "chair"
+            self.file_listbox.itemconfig(index, {'bg': 'lightgreen'})
+
+        self.file_listbox.selection_clear(0, tk.END)
+        messagebox.showinfo("Marked as Sittable", f"Marked {len(selected_files)} items as sittable.")
+
+    def mark_as_non_chairs(self):
+        selected_files = self.file_listbox.curselection()
+        for index in selected_files:
+            item_name = self.file_listbox.get(index)
+            self.items[item_name] = "non-chair"
+            self.file_listbox.itemconfig(index, {'bg': 'lightcoral'})
+
+        self.file_listbox.selection_clear(0, tk.END)
+        messagebox.showinfo("Marked as Items", f"Marked {len(selected_files)} items as non-sittable items.")
+
+    def generate_config_file(self):
+        file_name = simpledialog.askstring("Input", "Enter name for config file (do not include \".yml\"):")
+        if not file_name:
+            messagebox.showerror("Error", "No file name provided!")
+            return
+
+        self.custom_model_data = simpledialog.askinteger("Input", "Enter starting custom model data number:", initialvalue=0)
+        if self.custom_model_data is None:
+            messagebox.showerror("Error", "No custom model data number provided!")
+            return
+
+        folder_path = filedialog.askdirectory(title="Select Folder to Save Config File")
+        if folder_path:
+            full_file_path = os.path.join(folder_path, f"{file_name}.yml")
+
+            with open(full_file_path, 'w') as yaml_file:
+                for item_name, item_type in self.items.items():
+                    yaml_data = self.generate_yaml(item_name, item_type)
+                    yaml_file.write(yaml_data)
+
+            messagebox.showinfo("Config Generation", "Config file has been successfully generated!")
+
+    def generate_yaml(self, item_name, item_type):
+        display_name = item_name.replace("_", " ").title()
+        custom_model_data = self.custom_model_data
+        self.custom_model_data += 1
+
+        folder_name = os.path.basename(self.selected_folder)
+        model_path = f"civicraft/{folder_name}" # CHANGE: Change this to whatever folder in the pack folder you are putting it in.
+
+        if item_type == "chair":
+            return sittable_template.format(item_name=item_name, display_name=display_name, custom_model_data=custom_model_data, model_path=model_path)
+        else:
+            return item_template.format(item_name=item_name, display_name=display_name, custom_model_data=custom_model_data, model_path=model_path)
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = OraxenConfigGen(root)
+
+    root.mainloop()
